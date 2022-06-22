8a327c8c6cafdb3df521b7b0dbaded69c1df3e26
septemblue <focusramxram@gmail.com>
2022-06-22 09:04:55 +0700

    Logres FIrebase Auth

---
+++ app/src/main/java/com/example/jokol/Account.java
@@ -0,0 +1,13 @@
+package com.example.jokol;
+
+public class Account {
+    public String email;
+    public String password;
+    public String phone;
+
+    public Account(String email, String password, String phone) {
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+    }
+}
--- app/src/main/java/com/example/jokol/Register.java
+++ app/src/main/java/com/example/jokol/Register.java
@@ -1,16 +1,63 @@
 package com.example.jokol;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.jokol.databinding.ActivityRegisterBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import septemblue.example.jokionline.R;
 
 public class Register extends AppCompatActivity {
 
+    ActivityRegisterBinding binding;
+    FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        mAuth = FirebaseAuth.getInstance();
+
+        binding.registerBtn.setOnClickListener(it -> {
+
+
+            String email = binding.registerEmail.getText().toString();
+            String password = binding.registerPassword.getText().toString();
+            String phone = binding.registerPhone.getText().toString();
+
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Log.d("User", "createUserWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                    }else {
+                        Log.w("User", "createUserWithEmail:failure", task.getException());
+                        Toast.makeText(Register.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+        });
+
+        binding.loginRedirect.setOnClickListener(it -> {
+            finish();
+        });
     }
 }
